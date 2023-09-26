894bb579008a4ddbee7ee27f84f6b4bf5e0b1c3b
johnryanmal <johnryanmal@gmail.com>
2023-09-25 18:44:31 -0700

    redirect to root

--- src/router/routes.js
+++ src/router/routes.js
@@ -7,12 +7,9 @@ const routes = [
       { path: '', component: () => import('pages/IndexPage.vue') }
     ]
   },
-
-  // Always leave this as last one,
-  // but you can also remove it
   {
     path: '/:catchAll(.*)*',
-    component: () => import('pages/ErrorNotFound.vue')
+    redirect: '/',
   }
 ]
 
